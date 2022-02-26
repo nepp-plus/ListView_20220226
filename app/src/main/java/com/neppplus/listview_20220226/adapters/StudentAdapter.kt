270fedd931b64a94b19c0afa5caad8d249c57f3c
nepp.plus <nepp.plus@gmail.com>
2022-02-26 15:55:23 +0900

    각각의 줄에 뿌려질 데이터 / 반영할 텍스트뷰 들 가져오기

--- app/src/main/java/com/neppplus/listview_20220226/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20220226/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.neppplus.listview_20220226.R
 import com.neppplus.listview_20220226.datas.StudentData
 
@@ -27,7 +28,23 @@ class StudentAdapter(
 
         val row =  tempRow!!
 
+//        각 row의 UI에 > 자리에 맞는 사람 데이터를 반영.
 
+//        mList 변수에 => 액티비티에서 적어넣은 실제 학생 목록이 들어있다.
+
+        val data =  mList[ position ]
+
+//        row의 텍스트뷰 등등에, data 변수가 가진 하위 정보들을 반영
+
+//        row의 UI태그들을 => 직접 변수로 만들어내야함.
+        val txtStudentName = row.findViewById<TextView>(R.id.txtStudentName)
+        val txtAge = row.findViewById<TextView>(R.id.txtAge)
+        val txtAddress = row.findViewById<TextView>(R.id.txtAddress)
+
+
+
+
+//        반영된 채로 결과로 나가게
         return row
     }
 
