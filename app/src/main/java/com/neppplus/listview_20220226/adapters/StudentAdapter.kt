71ca60d99db12a27872fc9954c5a0ace8aaa54a4
nepp.plus <nepp.plus@gmail.com>
2022-02-26 15:01:37 +0900

    StudentAdapter가, 세가지 재료를 받아오도록 설정 > 부모클래스(ArrayAdapter)에게 전달

--- app/src/main/java/com/neppplus/listview_20220226/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20220226/adapters/StudentAdapter.kt
@@ -1,7 +1,12 @@
 package com.neppplus.listview_20220226.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.neppplus.listview_20220226.datas.StudentData
 
-class StudentAdapter : ArrayAdapter<StudentData>() {
+class StudentAdapter(
+    val mContext : Context,  // 어느 화면에서?
+    val resId: Int, // 어떤 xml을 기반으로?
+    val mList : ArrayList<StudentData>  // 실제로 뿌려줄 학생들은 누구누구? 목록
+) : ArrayAdapter<StudentData>(mContext, resId, mList) {
 }
