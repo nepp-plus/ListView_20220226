6867cfee0028e3018553b5102563d50de971399a
nepp.plus <nepp.plus@gmail.com>
2022-02-26 15:12:26 +0900

    student_list_item xml파일을, 코틀린에서 활용할 수 있는 준비. (tempRow에 담아두기)

--- app/src/main/java/com/neppplus/listview_20220226/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20220226/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.listview_20220226.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.listview_20220226.R
 import com.neppplus.listview_20220226.datas.StudentData
 
 class StudentAdapter(
@@ -9,4 +13,19 @@ class StudentAdapter(
     val resId: Int, // 어떤 xml을 기반으로?
     val mList : ArrayList<StudentData>  // 실제로 뿌려줄 학생들은 누구누구? 목록
 ) : ArrayAdapter<StudentData>(mContext, resId, mList) {
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow  = convertView
+
+        if (tempRow == null) {
+
+            tempRow =  LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
+
+        }
+
+    }
+
+
 }
