d2059ecdbff37f2764eba6c71f709e1dd15a294c
nepp.plus <nepp.plus@gmail.com>
2022-02-26 14:45:48 +0900

    학생 한명을 표현하는 데이터 클래스 추가  - 이름, 출생년도, 거주지를 하위 정보로

--- app/src/main/java/com/neppplus/listview_20220226/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20220226/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.listview_20220226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/neppplus/listview_20220226/datas/StudentData.kt
@@ -0,0 +1,8 @@
+package com.neppplus.listview_20220226.datas
+
+class StudentData(
+    val name: String,
+    val birthYear: Int,
+    val address: String,
+) {
+}
