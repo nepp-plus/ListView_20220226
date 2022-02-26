bbc11945366b3c016545f4fd128aa4bf1f9f0cfb
nepp.plus <nepp.plus@gmail.com>
2022-02-26 14:56:25 +0900

    어댑터 클래스 추가 제작 중  - ArrayAdapter를 기본 모양으로 상속 받았더니, 에러 발생

---
+++ app/src/main/java/com/neppplus/listview_20220226/adapters/StudentAdapter.kt
@@ -0,0 +1,7 @@
+package com.neppplus.listview_20220226.adapters
+
+import android.widget.ArrayAdapter
+import com.neppplus.listview_20220226.datas.StudentData
+
+class StudentAdapter : ArrayAdapter<StudentData>() {
+}
